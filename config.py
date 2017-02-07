8ba6052f3056e8b88be9d0132e3bb04b3efea614
A <adrian@acme.com>
2017-02-07 23:55:32 +0000

    minor change in config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
-f = os.popen('mv -f /bootflash/autoconfig/adriani/running.latest /home/guestshell/ciscolive-2017-nxos-workshops/running')
+f = os.popen('mv -f /bootflash/autoconfig/adriani/running.latest /home/guestshell/ciscolive-2017-nxos-workshop/running')
 who = f.read()
 f.close
 print "Result:", who
-f = os.popen('cd /home/guestshell/ciscolive-2017-nxos-workshops; git add running')
+f = os.popen('cd /home/guestshell/ciscolive-2017-nxos-workshop; git add running')
 who = f.read()
 f.close
 print "Result:", who
-f = os.popen('cd /home/guestshell/ciscolive-2017-nxos-workshops; git commit -m "Latest change"')
+f = os.popen('cd /home/guestshell/ciscolive-2017-nxos-workshop; git commit -m "Latest change"')
 who = f.read()
 f.close
 print "Result:", who
-f = os.popen('cd /home/guestshell/ciscolive-2017-nxos-workshops; chvrf management git push')
+f = os.popen('cd /home/guestshell/ciscolive-2017-nxos-workshop; chvrf management git push')
 who = f.read()
 f.close
 print "Result:", who
